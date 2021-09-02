8f482af755d60caab8d607e95f6d2816ba46fcc7
Dolores DIAZ <diaz.dolores@gmail.com>
2021-09-02 23:20:58 +0200

    Décomposition du projet

---
+++ core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.conf import settings
+
+
+class Ticket(models.Model):
+    ticket = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048, blank=True)
+    user = models.ForeignKey(
+                            to=settings.AUTH_USER_MODEL,
+                            on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+# Etendre le user de base 
