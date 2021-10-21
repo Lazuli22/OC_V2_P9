dbbc98343ba3c9f63e0cbd175cc1acc512110f53
Dolores DIAZ <diaz.dolores@gmail.com>
2021-10-21 23:50:00 +0200

    suppression d'un post

--- review/urls.py
+++ review/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url('modify/(?P<post_id>.*)', views.modify_post, name="modify_post"),
+    url('delete/(?P<post_id>.*)', views.delete_post, name="delete_post"),
     url('', views.posts, name="posts"),
 ]
